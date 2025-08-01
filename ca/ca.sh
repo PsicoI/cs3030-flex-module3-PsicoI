6249c7bd9c5cacb8294467f131625f643fe6a610
PsicoI <cody.turek@hotmail.com>
2025-08-01 14:12:57 -0600

    Fixed -g so it now works with -u and stdin. #TODO fix -f to work with -g like stdin. Refactored code to have consistent white space

--- ca/ca.sh
+++ ca/ca.sh
@@ -19,11 +19,24 @@ url=""
 filename=""
 search_word=""
 
+# # Standard Input # #
+# Accept piped input from STDIN if no filename is set and input is not a terminal
+if [[ -z "$filename" && ! -t 0 ]]; then
+    filename="/dev/stdin"
+fi
+# Saves STDN in to a temp file so all stats can perform their functions
+if [[ "$filename" == "/dev/stdin" ]]; then
+    tmpfile=$(mktemp)
+    cat > "$tmpfile"
+    filename="$tmpfile"
+    trap 'rm -f "$tmpfile"' EXIT
+fi
+
 
 ###
 #~~~~~~~~~~~~~~~~~~~~~~~~~~GET OPTS SECTION~~~~~~~~~~~~~~~~~~~~~~~~~~#
 # defines available options
-optstring="ahfwvcpdtTu:g:W:"
+optstring="ahfwvcpdtTu:gW:"
 while getopts "$optstring" options; do
     case "$options" in 
 # -c Pulls Consonant count statistics
@@ -48,6 +61,7 @@ while getopts "$optstring" options; do
                         -f FILE read from FILE instead of stdin. ***NOTE*** Cannot be combined with -u
                         -g input is from Project Gutenberg (ignore all lines before the start of the project and after the end of the project)
                         -h Display a help message
+                        -l counts the number of lines
                         -p output the punctuation count
                         -t MOST-USED output the top 10 MOST frequently used words and their counts
                         -T LEAST-USED output the top 10 LEAST frequently used words and their counts
@@ -58,7 +72,11 @@ while getopts "$optstring" options; do
             exit 0 
         ;;
     
-
+# -l Outputs the count of lines in
+        l)
+            do_line=true
+            echo "Line counter selected."
+        ;;
 
 # -p Outputs all punctuation statistics
         p)
@@ -77,22 +95,24 @@ while getopts "$optstring" options; do
         ;;
 # -u URL input (not specific to gutenberg)
         u)
-            do_url=true 
+            do_url=true
             url="$OPTARG"
             echo "URL: $url"
         ;;
+
+        # -g accepts Gutenberg project URL input out of alphabetical order to allow for -g and -f to have stats defined earlier in the script
+        g) 
+            do_gutenberg=true
+        ;;
+        
 # -v Pulls all vowel count statistic
         v)
             do_vowel=true
-            ;;
-# -g accepts Gutenberg project URL input out of alphabetical order to allow for -g and -f to have stats defined earlier in the script
-        g) 
-            do_gutenberg=true
-            url="$OPTARG"
         ;;
+
 # -f Expects and accepts file input
         f)
-
+            do_file=true
             filename=$OPTARG
             echo "File: $filename";
             echo "File selected"
@@ -118,31 +138,23 @@ while getopts "$optstring" options; do
   esac
 done
 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~END OF GET OPTS SECTION~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~START OF SYSTEM CHECKS~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
 # checks for file parameter and sets it operand for most flags
 shift $((OPTIND - 1))
 
-# Use OPTARG if set by -f, otherwise use positional argument if present
 if [[ -z "$filename" && -n "$OPTARG" && -f "$OPTARG" ]]; then
     filename="$OPTARG"
 elif [[ -z "$filename" && -n "$1" && -f "$1" ]]; then
     filename="$1"
 fi
 
-# -g function:
-if [[ $do_gutenberg == true ]]; then
-    echo "Commencing pull from $url"
-    filename=$(basename "$url")
-    curl -s "$url" | awk '/\*\*\* START OF THE PROJECT/ {flag=1; next} /\*\*\* END OF THE PROJECT/ {flag=0} flag' > "$filename"
-    if [[ $? -ne 0 || ! -s "$filename" ]]; then
-        echo "Download failed or file is empty."
-        exit 1
-    fi
-    echo "Saved to $filename"
-    echo -e "\ndone!"
-fi
 
-# -u function
-if [[ $do_url == true ]]; then
+# Only run -u logic if NOT using stdin
+if [[ "$filename" != "/dev/stdin" && $do_url == true && $do_gutenberg == true ]]; then
     echo "Commencing pull from $url"
     filename=$(basename "$url")
     curl -s "$url" > "$filename"
@@ -154,36 +166,132 @@ if [[ $do_url == true ]]; then
     echo -e "\ndone!"
 fi
 
+# Gutenberg logic + URL
+# if [[ "$filename" != "/dev/stdin" && $do_url == true && $do_gutenberg == true ]]; then
+#     if [[ -n "$filename" && -f "$filename" ]]; then
+#         echo "Processing $filename with Gutenberg AWK filter"
+#         tmpfile=$(mktemp)
+#         filename=$(basename "$url")
+#         curl -s "$url" > "$filename"
+#         awk '/\*\*\* START OF THE PROJECT/ {flag=1; next} /\*\*\* END OF THE PROJECT/ {flag=0} flag' "$filename" > "$tmpfile"
+#         if [[ ! -s "$tmpfile" ]]; then
+#             echo "Warning: Gutenberg filter produced no output. File not overwritten."
+#             rm "$tmpfile"
+#         else
+#             mv "$tmpfile" "$filename"
+#             echo "Filtered and saved to $filename"
+#         fi
+#         echo -e "\ndone!"
+#     else
+#         echo "No file specified or file does not exist."
+#         exit 1
+#     fi
+# fi
+
+# Download file if -u is set (and not using stdin)
+if [[ "$filename" != "/dev/stdin" && $do_url == true ]]; then
+    echo "Commencing pull from $url"
+    filename=$(basename "$url")
+    curl -s "$url" > "$filename"
+    if [[ $? -ne 0 || ! -s "$filename" ]]; then
+        echo "Download failed or file is empty."
+        exit 1
+    fi
+    echo "Saved to $filename"
+    echo -e "\ndone!"
+fi
+
+# Apply Gutenberg filter if requested (after download or file assignment)
+if [[ "$filename" != "/dev/stdin" && $do_gutenberg == true ]]; then
+    if [[ -n "$filename" && -f "$filename" ]]; then
+        echo "Processing $filename with Gutenberg AWK filter"
+        tmpfile=$(mktemp)
+        awk '/\*\*\* START OF THE PROJECT/ {flag=1; next} /\*\*\* END OF THE PROJECT/ {flag=0} flag' "$filename" > "$tmpfile"
+        if [[ ! -s "$tmpfile" ]]; then
+            echo "Warning: Gutenberg filter produced no output. File not overwritten."
+            rm "$tmpfile"
+        else
+            mv "$tmpfile" "$filename"
+            echo "Filtered and saved to $filename"
+        fi
+        echo -e "\ndone!"
+    else
+        echo "No file specified or file does not exist."
+        exit 1
+    fi
+fi
+
+
+# Gutenberg logic + filename
+if [[ "$do_file" == true && "$do_gutenberg" == true ]]; then
+    if [[ -n "$filename" && -f "$filename" ]]; then
+        echo "Processing $filename with Gutenberg AWK filter"
+        tmpfile=$(mktemp)
+        awk '/\*\*\* START OF THE PROJECT/ {flag=1; next} /\*\*\* END OF THE PROJECT/ {flag=0} flag' "$filename" > "$tmpfile"
+        if [[ ! -s "$tmpfile" ]]; then
+            echo "Warning: Gutenberg filter produced no output. File not overwritten."
+            rm "$tmpfile"
+        else
+            mv "$tmpfile" "$filename"
+            echo "Filtered and saved to $filename"
+        fi
+        echo -e "\ndone!"
+    else
+        echo "No file specified or file does not exist."
+        exit 1
+    fi
+fi
+
+
+# Now check for file existence (but skip if using /dev/stdin)
+if [[ "$filename" != "/dev/stdin" && ! -f "$filename" ]]; then
+    echo "No file specified or file does not exist."
+    exit 1
+fi
+
 if [[ -z "$filename" ]]; then
     echo "No file specified or file does not exist."
     exit 1
 fi
-#~~~~~~~~~~~~~~~~~~~~~~~~~~END OF GET OPTS SECTION~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
+
+#~~~~~~~~~~~~~~~~~~~~~~~~START OF INPUT CHECK SECTION~~~~~~~~~~~~~~~~~~~~~~~~#
 # checks to see if argument options have been combined and flags them as mutually exclusive
-
-
-
-# Checks to make sure that only URL, G-URL, or file is being sent as input
-if [[ "$do_file" == true && ( "$do_url" == true || "$do_gutenberg" == true ) ]]; then
-    echo "Options -f, -u, and -g are mutually exclusive."
+# Checks to make sure that only URL or file is being sent as input
+if [[ "$do_file" == true && "$do_url" == true  ]]; then
+    echo "Options -f, and -u are mutually exclusive."
     exit 1
 fi
+
+if [[ -z "$filename" && ! -t 0 ]]; then
+    filename="/dev/stdin"
+fi
+
+if [[ -z "$filename" ]]; then
+    echo "No file specified or file does not exist."
+    exit 1
+fi
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~END OF INPUT CHECK SECTION~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+
 
 # If no stats flags are set, default to all stats
 if [[ "$do_consonant" == false && "$do_digit" == false && "$do_most" == false && "$do_least" == false && "$do_punct" == false && "$do_vowel" == false && "$do_wordcount" == false && "$do_wordsearch" == false ]]; then
     # If no file/URL flags are set, read from stdin and set do_all_stats
 
      do_all_stats=true
+
 fi
 
-    if [[ -z "$filename" && "$do_url" == false && "$do_gutenberg" == false ]]; then
+if [[ -z "$filename" && "$do_url" == false && "$do_gutenberg" == false ]]; then
         filename=$OPTARG
         do_all_stats=true
-    fi
+fi
 
 # -a Sets ALL STATS TRUE
     if [[ $do_all_stats == true ]]; then
+            do_line=true
             do_consonant=true
             do_digit=true
             do_most=true
@@ -191,66 +299,33 @@ fi
             do_punct=true
             do_vowel=true
             do_wordcount=true
+
+
     fi
 
+# -l Line count IF STATEMENT
+    if [[ $do_line == true ]]; then
+        echo "Line count: "
+        grep -c '' "$filename"
 
-# -g function:
-    if [[ $do_gutenberg == true ]]; then
-        echo "Commencing pull from $url"
-
-        # Extract filename from URL (e.g., pg76386.txt)
-        filename=$(basename "$url") 
-
-            # pulls in text from a Gutenberg project URL
-            curl -s "$url" |
-            # ommits the header and footer
-            awk '/\*\*\* START OF THE PROJECT/ {flag=1; next} /\*\*\* END OF THE PROJECT/ {flag=0} flag' > "$filename"  
-            # checks if the file was downloaded successfully
-            if [[ $? -ne 0 || ! -s "$filename" ]]; then
-                echo "Download failed or file is empty."
-                exit 1
-            fi
-            echo "Saved to $filename"
-            # -- END OF -G --
-
-        echo -e "\ndone!"
     fi
 
-# -u function
-    if [[ $do_url == true ]]; then
-        echo "Commencing pull from $url"
-
-        # Extract filename from URL (e.g., pg76386.txt)
-        filename=$(basename "$url")
-
-            # pulls in text from a URL
-            curl -s "$url" > "$filename"
-            # checks if the file was downloaded successfully
-            if [[ $? -ne 0 || ! -s "$filename" ]]; then
-                echo "Download failed or file is empty."
-                exit 1
-            fi
-
-            echo "Saved to $filename"
-            # -- END OF -U --
-
-        echo -e "\ndone!"
-    fi
 
 # -c Consonant stat IF STATEMENT
     if [[ $do_consonant == true ]]; then
         # Output consonant count (including y)
             echo -e "Consonant count (including y) in $filename:"
             grep -o -i '[bcdfghjklmnpqrstvwxyz]' "$filename" | wc -l
-            echo -e "\n"
+    
 
     fi
-# -d Digit stat IF STATEMENT
-    if [[ $do_digit == true ]]; then
-        # Output digit count
-            echo -e "Digit count in $filename:"
-            tr -cd '[:digit:]' < "$filename" | wc -c
-            echo -e "\n"
+
+ # -v Vowel stat IF STATEMENT
+    if [[ $do_vowel == true ]]; then
+        # Output vowel count (ignoring y)
+            echo -e "Vowel count in $filename:"
+            grep -o -i '[aeiou]' "$filename" | wc -l
+    
     fi
 
 # -p Punctuation stat IF STATEMENT
@@ -258,8 +333,16 @@ fi
         # Output punctuation count
             echo -e "Number of Punctuation marks in $filename:"
             tr -cd '[:punct:]' < "$filename" | wc -c
-            echo -e "\n"
+    
             
+    fi
+
+# -d Digit stat IF STATEMENT
+    if [[ $do_digit == true ]]; then
+        # Output digit count
+            echo -e "Digit count in $filename:"
+            tr -cd '[:digit:]' < "$filename" | wc -c
+    
     fi
 
 
@@ -268,42 +351,40 @@ fi
         # Top 10 most frequent words and their counts
         echo -e "Top 10 most frequent words in $filename:"
         tr -cs '[:alnum:]' '[\n*]' < "$filename" | tr '[:upper:]' '[:lower:]' | sort | uniq -c | sort -nr | head -10
-        echo -e "\n"
+
     fi
 
 # -T Least used words stat IF STATEMENT
     if [[ $do_least == true ]]; then
     # Top 10 least frequent words and their counts
         echo -e "Top 10 least frequent words in $filename:"
-        tr -cs '[:alnum:]' '[\n*]' < "$filename" | tr '[:upper:]' '[:lower:]' | sort | uniq -c | sort -n | head -10
+        tr -cs '[:alpha:]' '[\n*]' < "$filename" | tr '[:upper:]' '[:lower:]' | sort | uniq -c | sort -n | head -10
         
 
     fi
-# -v Vowel stat IF STATEMENT
-    if [[ $do_vowel == true ]]; then
-        # Output vowel count (ignoring y)
-            echo -e "Vowel count in $filename:"
-            grep -o -i '[aeiou]' "$filename" | wc -l
-            echo -e "\n"
-    fi
 
-    # Word count for entire file
-if [[ "$do_wordcount" == true ]]; then
-    echo "Word count in $filename:"
-    wc -w < "$filename"
-    echo
-fi
+# Word count for entire file
+    if [[ "$do_wordcount" == true ]]; then
+        echo "Word count in $filename:"
+        wc -w < "$filename"
+        echo
+    fi
 
 # Word count for a specific word
-if [[ "$do_wordsearch" == true ]]; then
-    if [[ -z "$search_word" ]]; then
-        echo "No word specified for -W option."
-        exit 1
+    if [[ "$do_wordsearch" == true ]]; then
+        if [[ -z "$search_word" ]]; then
+            echo "No word specified for -W option."
+            exit 1
+        fi
+        echo "Count of '$search_word' in $filename:"
+        grep -o -w -i "$search_word" "$filename" | wc -l
+        echo
     fi
-    echo "Count of '$search_word' in $filename:"
-    grep -o -w -i "$search_word" "$filename" | wc -l
-    echo
-fi
 
 echo -e "\ndone!"
 exit 0
+
+
+
+# TODO: Fix -g not parsing files when -f is used. Try with romeo_and_juliet.txt
+# TODO: Fix 
